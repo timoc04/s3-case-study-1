7134ae6126d572ecdb2106becf56eab4702725cf
timo <timo.claessens@student.fontys.nl>
2026-09-20 21:35:24 +0200

    Add example variables for AWS region, VPC CIDR, and project name in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,14 @@
+variable "aws_region" {
+  description = "AWS region used for the infrastructure"
+  default     = "eu-central-1"
+}
 
+variable "vpc_cidr" {
+  description = "CIDR range of the Innovatech VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "project_name" {
+  description = "Name used for AWS resources"
+  default     = "innovatech-cs1"
+}
