fa29940fbfc7770a1be62be1d4ffcf823bf5ca2d
timo <timo.claessens@student.fontys.nl>
2026-09-21 09:51:26 +0200

    Add VPC and subnet configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,193 @@
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+
+# Public Subnets
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-b"
+  }
+}
+
+
+# Private Web Subnets
+resource "aws_subnet" "web_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.10.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-web-a"
+  }
+}
+
+resource "aws_subnet" "web_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.20.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-web-b"
+  }
+}
+
+
+# Private Database Subnets
+resource "aws_subnet" "database_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.30.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-database-a"
+  }
+}
+
+resource "aws_subnet" "database_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.40.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-database-b"
+  }
+}
+
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+
+# Public Route Table Associations
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+
+  tags = {
+    Name = "${var.project_name}-nat-gateway"
+  }
+}
+
+
+# Private Web Route Table
+resource "aws_route_table" "web_private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-web-private-rt"
+  }
+}
+
+
+# Private Web Route Table Associations
+resource "aws_route_table_association" "web_a" {
+  subnet_id      = aws_subnet.web_a.id
+  route_table_id = aws_route_table.web_private.id
+}
+
+resource "aws_route_table_association" "web_b" {
+  subnet_id      = aws_subnet.web_b.id
+  route_table_id = aws_route_table.web_private.id
+}
+
+
+# Private Database Route Table
+resource "aws_route_table" "database_private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-database-private-rt"
+  }
+}
+
+
+# Private Database Route Table Associations
+resource "aws_route_table_association" "database_a" {
+  subnet_id      = aws_subnet.database_a.id
+  route_table_id = aws_route_table.database_private.id
+}
+
+resource "aws_route_table_association" "database_b" {
+  subnet_id      = aws_subnet.database_b.id
+  route_table_id = aws_route_table.database_private.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,71 @@
+# VPC
+output "vpc_id" {
+  description = "ID of the Innovatech VPC"
+  value       = aws_vpc.main.id
+}
 
+
+# Public Subnets
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+}
+
+
+# Private Web Subnets
+output "web_subnet_ids" {
+  description = "IDs of the private web subnets"
+  value = [
+    aws_subnet.web_a.id,
+    aws_subnet.web_b.id
+  ]
+}
+
+
+# Private Database Subnets
+output "database_subnet_ids" {
+  description = "IDs of the private database subnets"
+  value = [
+    aws_subnet.database_a.id,
+    aws_subnet.database_b.id
+  ]
+}
+
+
+# Internet Gateway
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+
+# NAT Gateway
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.main.id
+}
+
+output "nat_gateway_public_ip" {
+  description = "Public IP address used by the NAT Gateway"
+  value       = aws_eip.nat.public_ip
+}
+
+
+# Route Tables
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "web_private_route_table_id" {
+  description = "ID of the private web route table"
+  value       = aws_route_table.web_private.id
+}
+
+output "database_private_route_table_id" {
+  description = "ID of the private database route table"
+  value       = aws_route_table.database_private.id
+}
