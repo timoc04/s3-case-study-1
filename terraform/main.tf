8100de541c16e2784559e75bf1962232ddfdc812
timo <timo.claessens@student.fontys.nl>
2026-09-21 14:07:16 +0200

    Fix indentation in IAM instance profile block of web launch template

--- terraform/main.tf
+++ terraform/main.tf
@@ -336,8 +336,8 @@ resource "aws_launch_template" "web" {
   instance_type = "t3.small"
 
   iam_instance_profile {
-  name = aws_iam_instance_profile.web.name
-}
+    name = aws_iam_instance_profile.web.name
+  }
 
   vpc_security_group_ids = [
     aws_security_group.web.id
