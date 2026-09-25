d66fe2a35e1d69a88da72971209025fe3d7a56ad
timo <timo.claessens@student.fontys.nl>
2026-09-25 15:52:14 +0200

    Fix storage type for PostgreSQL RDS instance from gp3 to gp2, after talking to my technical teacher (Gleb).

--- terraform/database.tf
+++ terraform/database.tf
@@ -21,7 +21,7 @@ resource "aws_db_instance" "main" {
   instance_class = "db.t3.micro"
 
   allocated_storage = 20
-  storage_type      = "gp3"
+  storage_type      = "gp2"
   storage_encrypted = true
 
   db_name  = "innovatech"
